e05c8909a7e58419c41514a8280f0f35213deb59
MarcoMadeddu <marcomade99@gmail.com>
2022-10-14 11:24:20 +0200

    Create react App add useState and first function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface FormData{
+  username: string;
+  job: string;
+}
+
 function App() {
+  const [formData, setFormData] = useState<FormData>({username: 'guest' , job: 'freelance'});
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>){
+    setFormData({username: e.currentTarget.value, job: 'employee'})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container mt-2">
+      <pre>{JSON.stringify(formData)}</pre>
+   
+
+      {
+        /*
+            NEW: This input is always in sync with formData.username
+              and updates its value when you write 
+            */
+      }
+      <input
+        name="username"
+        type="text"
+        placeholder="Write your username"
+        onChange = {onChangeHandler}
+        value={formData.username}
+      />
+   </div>
   );
 }
 
