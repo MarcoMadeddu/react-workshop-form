653e794696dbddf352146007775952400812c1ec
MarcoMadeddu <marcomade99@gmail.com>
2022-10-14 12:19:25 +0200

    Add a new input on form

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,28 @@ import './App.css';
 interface FormData{
   username: string;
   job: string;
+  age: number;
 }
 
 function App() {
-  const [formData, setFormData] = useState<FormData>({username: '' , job: 'freelance'});
+  const [formData, setFormData] = useState<FormData>({username: '' , job: '',age:0});
 
 
   // const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {setFormData({username: e.currentTarget.value, job: 'employee'})}
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormData({ username: e.currentTarget.value, job: 'employee'})
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    setFormData({ 
+     ...formData,
+     [e.currentTarget.name]: e.currentTarget.value
+    })
   }
   
-  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) =>{
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement> ) =>{
     // PREVENT DEFAULT FOR AVOID REFRESH
     e.preventDefault();
     // GET ALL FORM STATE VALUES
     console.log(formData);
     // RESET FORM
-    setFormData({username: '', job: ''});
+    setFormData({username: '', job: '', age: 0 });
   }
 
   return (
@@ -42,6 +46,23 @@ function App() {
           onChange = {onChangeHandler}
           value={formData.username}
         />
+        <br/>
+        <input
+          name="age"
+          type="number"
+          placeholder="insert your Age"
+          onChange = {onChangeHandler}
+        />
+         <br/>
+        <select 
+          name="job" 
+          onChange={onChangeHandler}
+          value={formData.job}
+        >
+          <option value=""  disabled hidden>Select your Job</option>
+          <option value="freelance">Freelance</option>
+          <option value="employee">Employee</option>
+        </select>
 
         <button className="btn btn-primary ms-2" type="submit">SEND</button>
       </form>
